edc5205c0a1efe02ea3b1e7a531135204f7b2813
BK <bruno.klqueiroz@gmail.com>
2026-04-04 22:12:14 +0100

    adding new features and api implementation connected with stockmkt

---
+++ client/src/components/tradeview/TradingViewWidget.jsx
@@ -0,0 +1,50 @@
+// TradingViewWidget.jsx
+import React, { useEffect, useRef, memo } from 'react';
+
+function TradingViewWidget() {
+  const container = useRef();
+
+  useEffect(
+    () => {
+      const script = document.createElement("script");
+      script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+      script.type = "text/javascript";
+      script.async = true;
+      script.innerHTML = `
+        {
+          "allow_symbol_change": true,
+          "calendar": false,
+          "details": false,
+          "hide_side_toolbar": true,
+          "hide_top_toolbar": false,
+          "hide_legend": false,
+          "hide_volume": false,
+          "hotlist": false,
+          "interval": "D",
+          "locale": "en",
+          "save_image": true,
+          "style": "1",
+          "symbol": "NASDAQ:AAPL",
+          "theme": "dark",
+          "timezone": "Etc/UTC",
+          "gridColor": "rgba(242, 242, 242, 0.06)",
+          "watchlist": [],
+          "withdateranges": false,
+          "compareSymbols": [],
+          "studies": [],
+          "autosize": false
+        }`;
+      container.current.appendChild(script);
+    },
+    []
+  );
+
+  return (
+    <div className="tradingview-widget-container" ref={container} style={{ height: "100%", width: "100%", marginBottom: "10%" }}> </div>
+
+    
+
+  );
+}
+
+export default memo(TradingViewWidget);
