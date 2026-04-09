794660aaaf57268c96640e9a972729d40cc9b295
Twilight-code <erik.resman@student.griffith.ie>
2026-04-09 19:12:39 +0100

    Register Form

--- client/src/components/register.js
+++ client/src/components/register.js
@@ -1,30 +1,55 @@
 
-
+import {useState} from "react";
 
 export default function register(){
+    const [form, setForm] = useState({
+        email: "",
+        password: "",
+    });
+
+    const [message, setMessage] = useState("");
+
+    const handleChange = (event) => {
+        setForm({
+            ...form,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleAdd = async (event) => {
+        event.preventDefault();
+
+        const res = await fetch("api/routes/register",{
+            method: "POST",
+            body: JSON.stringify(form),
+        });
+
+        const data = await res.json();
+
+        if (res.ok) {
+            setMessage("User registered successfully!");
+            setForm({ email: "", password: "" });
+        } 
+        else {
+            setMessage(data.message);
+        }
+    }
 
     return (
-        <form onSubmit={handleAdd}>
-        <h1>Register as a new user</h1>
-        <input value={email} onChange={handleEmailChange} placeholder="Set Email" />
-        <br />
-        <input
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-            placeholder="Set Password"
-        />
-        <br />
-        <select 
-            type="dropdown"  
-            onChange={handleRoleChange} >
-            <option value="">--Please choose an option--</option>
-            <option value="admin" >Admin</option>
-            <option value="landlord">Landlord</option>
-            <option value="tenant">Tenant</option> 
-        </select>
-        <br />
-        <button type="submit">Login</button>
-        </form>
+        <div>
+            <form onSubmit={handleAdd}>
+                <h1>Register as a new user</h1>
+                <input value={form.email} onChange={handleChange} placeholder="Set Email" />
+                <br />
+                <input
+                    type="password"
+                    value={form.password}
+                    onChange={handleChange}
+                    placeholder="Set Password"
+                />
+                <button type="submit">Register</button>
+            </form>
+            <p>{message}</p>
+        </div>
     );
 }
