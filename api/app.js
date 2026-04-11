ea850b7fbf99874f0e8877ae42dbe2beb55e0e11
Twilight-code <erik.resman@student.griffith.ie>
2026-04-11 12:12:09 +0100

    Api updates

--- api/app.js
+++ api/app.js
@@ -10,6 +10,10 @@ var usersRouter = require('./routes/users');
 var app = express();
 const mongoose = require("mongoose");
 
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.log(err));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -25,9 +29,7 @@ app.use('/users', usersRouter);
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.log(err));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
