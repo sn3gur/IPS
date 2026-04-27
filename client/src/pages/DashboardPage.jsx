f680e9f47a91ef68cc951c2d4fb5f767429ff6fb
Twilight-code <erik.resman@student.griffith.ie>
2026-04-27 23:17:42 +0100

    Black Background

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import '../styles/main.scss'
 
 function DashboardPage() {
   const navigate = useNavigate()
--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import '../styles/main.scss'
 
 function SignupPage() {
   const navigate = useNavigate()
