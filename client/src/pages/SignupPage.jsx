7a37c6bc1c8ef1a515328463525e9285189056f9
Twilight-code <erik.resman@student.griffith.ie>
2026-04-28 21:54:04 +0100

    Register

--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -1,30 +1,92 @@
-import { useNavigate } from 'react-router-dom'
+import {useState} from "react";
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import Navbar from '../components/Navbar'
-import '../styles/main.scss'
 
-function SignupPage() {
-  const navigate = useNavigate()
+export default function SignupPage(){
+    // Form Fields Email and Password for now
+    const [form, setForm] = useState({
+        email: "",
+        password: "",
+    });
 
-  const handleSignup = (e) => {
-    e.preventDefault()
-    // TODO: api-call to POST /api/users/register
-    navigate('/dashboard')
-  }
+    // Message for confirmation
+    const [message, setMessage] = useState("");
 
-  return (
-    <div className="page debug">
-      <Navbar variant="brand" />
+    // Event handler for form value changes
+    const handleChange = (event) => {
+        setForm({
+            ...form,
+            [event.target.name]: event.target.value,
+        });
+    };
 
-      <main className="page-content debug-blue">
-        <h1>IPS</h1>
+    // Event handler for subbmiting form
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
-        <form onSubmit={handleSignup} className="debug-green">
-          {/* TODO input fields */}
-          <button type="submit">Confirm</button>
+        if (!form.email){
+            return setMessage("Missing email");
+        }
+        else if (!form.password){
+            return setMessage("Missing password");
+        }
+        else {
+            setMessage("");
+        }
+        try {
+            setMessage("before fetch");
+            // Sends form to server
+            const res = await fetch("http://localhost:5050/api/user/register",{
+                method: "POST",
+                headers: {
+                "Content-Type": "application/json",
+                },
+                body: JSON.stringify(form),
+            });
+            setMessage("before data");
+            // Awaits for a response from server
+            const data = await res.json();
+            setMessage("before handle");
+            // Handles response 
+            if (res.ok) {
+                setMessage("User registered successfully!");
+                setForm({ email: "", password: "" });
+            }
+            setMessage("complete"); 
+        }
+        catch {
+            // Sets the response message to show up if failed to register user
+            setMessage(data.message);
+
+            //Testing Purposes
+            setMessage("Something's wrong");
+        }
+    }
+    // How the form looks
+    return (
+      <div class="page">
+        <Navbar variant="back" balance={100000} />
+        <form onSubmit={handleSubmit}>
+            <h1>Register as a new user</h1>
+            <input 
+                type="email"
+                name="email"
+                value={form.email} 
+                onChange={handleChange} 
+                placeholder="Set Email" 
+            />
+            <br />
+            <input
+                type="password"
+                name="password"
+                value={form.password}
+                onChange={handleChange}
+                placeholder="Set Password"
+            />
+            <br/>
+            <button type="submit">Register</button>
+            <p>Info: {message}</p>
         </form>
-      </main>
-    </div>
-  )
+      </div>
+    );
 }
-
-export default SignupPage
