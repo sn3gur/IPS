8d6bb8666ba2e7c3ebcf3b20da40f8b5c7b197f7
sn3gur <asnigurak@gmail.com>
2026-05-09 23:00:42 +0100

    fixes

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.use(
 
 const corsOptions = {
   origin: process.env.NODE_ENV === 'production' 
-    ? 'https://ips-lbcs.onrender.com' 
+    ? 'https://ips-frontend-hfp6.onrender.com' 
     : 'http://localhost:5173',
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
